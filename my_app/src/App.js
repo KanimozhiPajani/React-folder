8f7d115575f8d40e0a05453b863a9da6c33f78eb
Kani <kanimozhipazhani338@gmail.com>
2021-01-12 21:46:27 +0530

    merged

--- my_app/src/App.js
+++ my_app/src/App.js
@@ -2,29 +2,13 @@ import logo from './logo.svg';
 import './App.css';
 import Joblist from './Component/List';
 import Topic from './Component/topic';
-<<<<<<< HEAD
-
-
-import RandomNameGen from './Component/RandomNameGen';
-
-function App() {
-  return (
-    <div className="App">
-
-      <header className ="App-header">
-       <RandomNameGen />
-      </header>
-      
-       {/*<Joblist />*/}
-=======
 import RandomNamePicker from './Component/RandomNamePicker';
 import Location from './Component_2/location';
 function App() {
   return (
     <div className="App">
-      <Location/>
-      {/*<RandomNamePicker />*/}
->>>>>>> Branch-1
+      {/*<Location/>*/}
+      <RandomNamePicker />
     </div>
   );
 }
