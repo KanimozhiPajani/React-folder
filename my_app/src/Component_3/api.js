76fc0259a33f2f62a5a9e0991f0e895a33e4c55b
Kani <kanimozhipazhani338@gmail.com>
2021-01-12 15:34:55 +0530

    created an Api request

--- my_app/src/Component_3/api.js
+++ my_app/src/Component_3/api.js
@@ -1,11 +1,17 @@
 import React, { Component } from 'react'
 import SearchBar from './searchBar'
+import axios from 'axios';
 
 export class Api extends Component {
 
-onSumbit = (term) => {
+onSumbit  (term) {
+    axios.get('https://api.unsplash.com/search/photos',{
+        params:{query:term},
+        headers:{Authorization:'Client-ID _julpuoRzF2I4AYywaA8b7HmzVadYGNaBoLq7Rge7GY'}
+    });
     console.log(term);
 }
+
 
     render() {
         return (
