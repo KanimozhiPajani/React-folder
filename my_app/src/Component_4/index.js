47f620ce9f3aa12de2ca6cde10a4cf76d181ba82
Kani <kanimozhipazhani338@gmail.com>
2021-02-10 01:37:58 +0530

    Structured

--- my_app/src/Component_4/index.js
+++ my_app/src/Component_4/index.js
@@ -18,9 +18,14 @@ constructor(props) {
 
     this.state = {
          data : [],
+         value:[],
          name:'',
          count:null,
-         num:null
+         currencies:'',
+         capital:'',
+         numbercode:null,
+         timeZone:null,
+         population:null
     }
     
 }
@@ -42,29 +47,60 @@ componentDidMount() {
  
   
 change = () =>{
-  this.count=[Math.floor(Math.random() * 20)];
-  this.setState({name:this.state.data[this.count].name})
-  console.log(this.state.data);
+  this.count=[Math.floor(Math.random() * 250)];
+  const {name,currencies,capital,numericCode,timezones,population}=this.state.value
+  this.setState({
+    value:this.state.data[this.count],
+    name:name,
+    currencies:currencies,
+    capital:capital,
+    numbercode:numericCode,
+    timeZone:timezones,
+    population:population  
+  })
+    console.log(this.state.data);
 }
 
 
 
 select =()=>{
+const {name,currencies,capital,numericCode,timezones,population}=this.state.value
+  
   this.setState({
-    
-    name:this.state.data[this.state.count].name})
+  value:this.state.data[this.state.count],
+  name:name,
+  currencies:currencies,
+  capital:capital,
+  numbercode:numericCode,
+  timeZone:timezones,
+  population:population
+  })
   }
 
     render() {
-
+const {name,currencies,numbercode,timeZone,capital,population}=this.state
         return (<div>
   <h1>Hello World</h1>
   <button onClick={this.change}>Click</button>
-  <h1>{this.state.name}</h1>
-  <button onClick={()=>this.setState({count:1})}>Aglend</button>
-  <button onClick={()=>this.setState({count:2})}>Aruba</button>
-  <button onClick={()=>this.setState({count:3})}>America</button>
-  <button onClick={()=>this.setState({count:4})}>Arabia</button>
+
+  <h1>{name}</h1>
+  <h1>{currencies}</h1>
+  <h1>{numbercode}</h1>
+  <h1>{timeZone}</h1>
+  <h1>{capital}</h1>
+  <h1>{population}</h1>
+
+  <button onClick={()=>this.setState({count:0})}>Afghanistan</button>
+  <button onClick={()=>this.setState({count:1})}>Åland Islands</button>
+  <button onClick={()=>this.setState({count:144})}>Mexico</button>
+  <button onClick={()=>this.setState({count:185})}>Russia</button>
+  <button onClick={()=>this.setState({count:202})}>Singapore</button>
+  <button onClick={()=>this.setState({count:104})}>India</button>
+  <button onClick={()=>this.setState({count:17})}>Bahrain</button>
+  <button onClick={()=>this.setState({count:48})}>China</button>
+  <button onClick={()=>this.setState({count:112})}>Italy</button>
+  <button onClick={()=>this.setState({count:114})}>Japan</button>
+  
   <button onClick={this.select}>Get</button>
 </div>
         )
